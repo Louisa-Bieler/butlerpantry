31bfa7e731450361e7eb77634c64e516182c5735
louisa.bieler <louisa.bieler@adevinta.com>
2023-04-25 16:13:55 +0200

    Refactoring PantryLogic to make internal parts "private" and separate the checking of a recipe against a pantry, subtracting a recipe from a pantry, creating a shopping List as a Pantry object, and then one public method that does the whole thing and writes the new shopping List to a CSV file

--- src/com/louisa/butlerpantry/PantryLogic.java
+++ src/com/louisa/butlerpantry/PantryLogic.java
@@ -4,12 +4,14 @@ import com.louisa.logging.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Scanner;
 
 public class PantryLogic {
 
     public static Pantry producePantryFromFile(File defaultPantryFile) throws IOException {
-        Pantry newPantry = new Pantry();
+        Pantry newPantry = new Pantry(defaultPantryFile.getName());
         try (Scanner scanner = new Scanner(defaultPantryFile).useDelimiter(",")) {
             while (scanner.hasNextLine()) {
                 Ingredient iterIngredient = IngredientLogic.returnIngredient(scanner.nextLine());
@@ -23,14 +25,18 @@ public class PantryLogic {
         }
     }
 
-    public static boolean checkRecipeAgainstPantry(Pantry toUpdate, Pantry recipe) {
-        Pantry shoppingList = new Pantry();
+    private static boolean checkRecipeAgainstPantry(Pantry toUpdate, Pantry recipe) {
+        return (toUpdate.getInventory().keySet().containsAll(recipe.getInventory().keySet()));
+    }
+
+    private static Pantry createShoppingList(Pantry toUpdate, Pantry recipe) {
+        Pantry shoppingList = new Pantry(recipe.getName() + Date.from(Instant.now()));
         recipe.getInventory().forEach(
                 (recipeIngredientKey, recipeIngredient) ->
                 {
-                    if(!toUpdate.getInventory().keySet().contains(recipeIngredientKey)){
+                    if (!toUpdate.getInventory().keySet().contains(recipeIngredientKey)) {
                         shoppingList.setIngredientFromIngredient(recipeIngredient);
-                    } else if (toUpdate.getIngredient(recipeIngredientKey).getAmount()>recipeIngredient.getAmount()) {
+                    } else if (toUpdate.getIngredient(recipeIngredientKey).getAmount().compareTo(recipeIngredient.getAmount()) == 1) {
                         return;
                     } else if (toUpdate.getIngredient(recipeIngredientKey).getAmount().equals(recipeIngredient.getAmount())) {
                         return;
@@ -39,37 +45,42 @@ public class PantryLogic {
                     }
                 }
         );
-        if (shoppingList.getInventory().isEmpty()){
-            return true;
-        } else {
-            WriteFile.createUpdatedCSV(shoppingList, "shoppingList.csv");
-            return false;
-        }
-
+        return shoppingList;
     }
 
-    public static void subtractPrecheckedRecipeFromPantry(Pantry toUpdate, Pantry recipe){
+    private static void subtractRecipeFromPantry(Pantry toUpdate, Pantry recipe) {
         recipe.getInventory().forEach(
                 (recipeIngredientKey, recipeIngredient) ->
-                { if (toUpdate.getIngredient(recipeIngredientKey).getAmount()>recipeIngredient.getAmount()) {
-                    toUpdate.getIngredient(recipeIngredientKey).subtractAmountFromRecipe(recipeIngredient.getAmount());
-                } else {
-                    toUpdate.removeEntireIngredient(recipeIngredientKey);
+                {
+                    if (toUpdate.getIngredient(recipeIngredientKey).getAmount().compareTo(recipeIngredient.getAmount()) == 1) {
+                        toUpdate.getIngredient(recipeIngredientKey).subtractAmountFromRecipe(recipeIngredient.getAmount());
+                    } else {
+                        toUpdate.removeEntireIngredient(recipeIngredientKey);
+                    }
                 }
-
-    });
-    }
-
-    public static void addShopping(Pantry toUpdate, Pantry entries) {
-        entries.getInventory().forEach(
-                (key, value) ->
-                        toUpdate.getInventory().merge(
-                                key, value, (value1, value2) ->
-                                {
-                                    value1.addAmountFromShopping(value2.getAmount());
-                                    return value1;
-                                }
-                        )
         );
     }
-}
+
+    public static void addRecipeIfPossibleCreateShoppingListIfNot(Pantry toUpdate, Pantry recipe) {
+        if (!checkRecipeAgainstPantry(toUpdate, recipe)) {
+            Pantry shoppingList = createShoppingList(toUpdate, recipe);
+            WriteFile.writeShoppingListToCSV(shoppingList.toString(), shoppingList.getName());
+        } else {
+            subtractRecipeFromPantry(toUpdate, recipe);
+        }
+    }
+
+        public static void addShopping (Pantry toUpdate, Pantry entries){
+            entries.getInventory().forEach(
+                    (key, value) ->
+                            toUpdate.getInventory().merge(
+                                    key, value, (value1, value2) ->
+                                    {
+                                        value1.addAmountFromShopping(value2.getAmount());
+                                        return value1;
+                                    }
+                            )
+            );
+        }
+    }
+
