6c48a419a471a25880f840d1d11a7106b5d6f73a
Louisa Lynne Bieler <louisa.lynne.bieler@gmail.com>
2023-05-02 13:13:54 +0200

    Adding Tests as TODO from IntelliJ Auto "Generate" tests

---
+++ src/com/louisa/test/com/louisa/butlerpantry/UnitConversionTest.java
@@ -0,0 +1,16 @@
+package com.louisa.butlerpantry;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitConversionTest {
+
+    @Test
+    void addUnitConversionMapping() {
+    }
+
+    @Test
+    void unitConversion() {
+    }
+}
